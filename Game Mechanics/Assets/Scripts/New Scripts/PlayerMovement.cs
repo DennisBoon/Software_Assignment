9d658ccccbe20d4ee16e14ec2a3bd3dbee82bab6
Dennis Boon <dennis.boon96@gmail.com>
2019-09-18 13:00:03 +0200

    New scripts

--- Game Mechanics/Assets/Scripts/New Scripts/PlayerMovement.cs
+++ Game Mechanics/Assets/Scripts/New Scripts/PlayerMovement.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 5.0f;
+    public float sprintSpeed = 10.0f;
+    public float jumpSpeed = 6.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    public bool controllable = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Player movement
+        if (controller.isGrounded && controllable)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            // Player speed
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                moveDirection *= sprintSpeed;
+            }
+            else
+            {
+                moveDirection *= speed;
+            }
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
